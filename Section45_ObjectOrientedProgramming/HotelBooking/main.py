ed5db7afdbf796f5b4c11c9d2bb73e6bb23fb83a
Cristian Nadj <Cris>
2024-05-30 13:53:03 -0400

    Working with Credit Card validations

--- Section45_ObjectOrientedProgramming/HotelBooking/main.py
+++ Section45_ObjectOrientedProgramming/HotelBooking/main.py
@@ -1,8 +1,10 @@
 import pandas
 
-# Initiating a Pandas Object with a method to read the .csv file. 
-# The dtype means that we're sending a String rather than an Integer for the ID of the Hotel.
+# Initiating a Pandas Object with a methods to read .csv files. 
+# The dtype means that we're sending a String rather than an Integer for the ID of the Hotel and a 
+# STR as part of the entire record for every one of those Hotels.
 df = pandas.read_csv("hotels.csv", dtype= {"id" : str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records") # Init the Object into a Dictionary to compare values.
 
 
 class Hotel:
@@ -26,6 +28,7 @@ class Hotel:
         else:
             return False
 
+
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
         self.customer_name = customer_name
@@ -42,7 +45,17 @@ class ReservationTicket:
 
 
 class CreditCard:
-    pass
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration, 
+                     "holder": holder, "cvc": cvc}
+        # Compare whether a specific Card is present as part of the cards.csv file.
+        if card_data in df_cards:
+            return True
+        else:
+            return False
 
 
 '''MAIN PROGRAM'''
@@ -54,13 +67,16 @@ hotel = Hotel(hotel_ID)
 
 if (hotel.available):
     # Asks Users for a specific Credit Card Number
-    credit_card = CreditCard()
-    
-    hotel.book()
-    name = input("Enter your name: ")
-    # Providing the Hotel ID and the Name of the person who reserves the Ticket
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard(number="123456") # Adding a harcoding one which corresponds with a record in the cards.csv file
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        # Providing the Hotel ID and the Name of the person who reserves the Ticket
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print("Goes here or not?")
+        print(reservation_ticket.generate())
+    else:
+        print("There was a problem with your payment. Please, try again!")
 else:
     print("Hotel is not free.")
 
@@ -69,4 +85,5 @@ Hotels.csv FILE
 id                     name        city         capacity   available
 0  134  Tourist Sunny Apartment   Anchorage         4       yes
 1  188              Snow Palace   New Delhi         5        no
-2  655           City Break Inn  Porto-Novo         3        no'''
+2  655           City Break Inn  Porto-Novo         3        no
+s'''
